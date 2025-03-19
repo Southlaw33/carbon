c2f3f9f191a0b7e9f8a3db3076ce5ebc1407b25c
Atchutha Rao T A <atchutha57@gmail.com>
2025-03-19 14:28:42 +0530

    Added methods to get enriched students and professor details

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,81 @@ app.get("/students", async (c) => {
   }
 });
 
+//Retrieving all the professors in the college
+app.get("/professors", async (c) => {
+  try {
+    const professors = await prisma.professor.findMany();
+    return c.json(professors, 200);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+//Getting enriched student records (has proctor details along with student)
+app.get("/students/enriched", async (c) => {
+  try {
+    const studentsEnriched = await prisma.student.findMany({
+      include: {
+        proctor: true,
+      },
+    });
+    return c.json(studentsEnriched, 200);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+//Adding new student record
+app.post("/students", async (c) => {
+  const { name, dateOfBirth, aadharNumber, proctorId } = await c.req.json();
+  try {
+    const studentAadhar = await prisma.student.findUnique({
+      where: {
+        aadharNumber,
+      },
+    });
+    if (studentAadhar) {
+      return c.json({ message: "Bad Request!" }, 400);
+    }
+
+    const student = await prisma.student.create({
+      data: {
+        name,
+        dateOfBirth,
+        aadharNumber,
+        proctorId,
+      },
+    });
+    return c.json(student, 201);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+//Adding a professor
+app.post("/professors", async (c) => {
+  const { name, seniority, aadharNumber } = await c.req.json();
+  try {
+    const existingProfessor = await prisma.professor.findUnique({
+      where: { aadharNumber },
+    });
+
+    if (existingProfessor) {
+      return c.json({ message: "bad request" }, 400);
+    }
+    const professor = await prisma.professor.create({
+      data: {
+        name,
+        seniority,
+        aadharNumber,
+      },
+    });
+    return c.json(professor, 201);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 //
 
 serve(app);
